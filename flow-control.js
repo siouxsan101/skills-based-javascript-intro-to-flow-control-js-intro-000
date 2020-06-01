4d96d61a9ece43cfb7aa8d0abd6a6be9a3249c7b
susan hofmann <susanhofmannnyc@icloud.com>
2020-06-01 20:51:09 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -3,21 +3,3 @@ function basicTeenager(age){
     return "You are a teenager!";
   }
 }
-
-function teenager(age){
-  if(age >=13 && age <= 19) {
-      return "You are a teenager!";
-  } else {
-      return "You are not a teenager";
-  }
-}
-
-function ageChecker(age) {
-  if(age >= 13 && age <= 19) {
-      return "You are a teenager!";
-  } else if (age < = 12){
-      return "You are a kid";
-  } else if (age >= 20){
-    return "You are a grownup";
-  }
-}
