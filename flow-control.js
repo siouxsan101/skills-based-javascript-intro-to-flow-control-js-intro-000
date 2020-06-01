b1025cb65c954c736c9bcb627ea75dd0a469b6fd
susan hofmann <susanhofmannnyc@icloud.com>
2020-06-01 22:43:03 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -21,3 +21,9 @@ function ageChecker(age) {
     return "You are a grownup";
   }
 }
+function ternaryTeenager(age){
+  return age >= 13 && age <= 19 ? 
+  "You are a teenager": 
+  "You are not a teenager"
+  }
+
