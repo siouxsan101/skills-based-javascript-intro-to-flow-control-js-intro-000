7d702f2def958dbd3fd75c5e30ced39f4dfbd6aa
susan hofmann <susanhofmannnyc@icloud.com>
2020-06-01 20:45:09 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -15,10 +15,10 @@ function ageChecker(age){
   if(age >= 13 && age <= 19){
       return "You are a teenager!";
   }
-  if else (age < = 12){
+  else if (age < = 12){
       return "You are a kid";
   }
-  if else (age >= 20){
+  else if (age >= 20){
     return "You are a grownup";
   }
 }
