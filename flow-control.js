1fa44f95022dfe31209a1d919e7eabafe499aba9
susan hofmann <susanhofmannnyc@icloud.com>
2020-06-01 19:21:11 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,11 +29,11 @@ function ternaryTeenager(age){
 }
 
 function switchAge(age){
-  switch(age) {
+   {
     case age >= 13 && age <= 19:
       return "You are a teenager"
-      break;
     case age < 13 && age > 19:
       return "You have an age"
     }
   }
+  
